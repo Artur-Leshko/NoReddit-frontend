27223357af8e97d66d9f0276094256c1ae9d6533
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-13 01:02:20 +0300

    implement routing with react-router-dom

---
+++ src/components/App/AppRouter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate, } from 'react-router-dom';
+import { NotFound, ProtectedRoute, } from '../../common';
+import { Layout, } from '../Layout';
+import { Login, Register, } from '../User';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path='/' element={<Navigate to='noreddit' replace />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='noreddit' element={<div>Hi!</div>} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
