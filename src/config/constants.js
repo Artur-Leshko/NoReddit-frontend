99fd1ce917cdbbe183a6f4e8a7652c82d40040cc
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-13 00:25:51 +0300

    add constants for auth

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,4 @@
 const AUTH_TOKEN_KEY = 'access';
 const REFRESH_TOKEN_KEY = 'refresh';
-const REFRESH_TOKEN_URL = 'http://localhost:3000/api/v1/auth/regresh/';
+const REFRESH_TOKEN_URL = 'http://localhost:8000/api/v1/auth/regresh/';
 export { AUTH_TOKEN_KEY, REFRESH_TOKEN_KEY, REFRESH_TOKEN_URL, };
