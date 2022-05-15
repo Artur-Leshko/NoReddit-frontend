82cc7f550bab3f9d892d4aac7928e3c5114f5f14
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-15 22:58:39 +0300

    update layout for Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,42 +8,47 @@ export const Header = () => {
   return (
     <header className='header'>
       <div className='container'>
-        <div className='header__logo'>
-          <Link to='/'>NOReddit</Link>
-        </div>
-        <nav className='header__navigation'>
-          <ul>
-            <li>
-              <Link className='header__navigation-link'>Do</Link>
-            </li>
-            <li>
-              <Link className='header__navigation-link'>Re</Link>
-            </li>
-            <li>
-              <Link className='header__navigation-link'>Me</Link>
-            </li>
-            <li>
-              <Link className='header__navigation-link'>Fa</Link>
-            </li>
-            <li>
-              <Link className='header__navigation-link'>Sol</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className='header__user'>
-          <div className='header__user-img'>
-            <Link>
-              <img src={defaulAvatar} alt='default user avatar' />
+        <div className='header__inner'>
+          <div className='header__logo'>
+            <Link to='/'>
+              <span className='header__logo-white'>N</span>
+              <span className='header__logo-red'>O</span>
+              <span className='header__logo-white'>R</span>
+              <span className='header__logo-black'>e</span>
+              <span className='header__logo-black'>d</span>
+              <span className='header__logo-white'>d</span>
+              <spane className='header__logo-red'>i</spane>
+              <span className='header__logo-white'>t</span>
             </Link>
           </div>
-          <div className='header__user-username'>MyName</div>
-          <Button
-            style={ButtonStyles.DELETE}
-            kind={ButtonKinds.INFO}
-            className='header__user-logout'
-          >
-            Logout
-          </Button>
+          <nav className='header__navigation'>
+            <ul className='header__navigation-list'>
+              <li>
+                <Link to='categories' className='header__navigation-link'>Post categories</Link>
+              </li>
+              <li>
+                <Link to='myposts' className='header__navigation-link'>My posts</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className='header__user'>
+            <div className='header__user-username'>
+              <Link to='profile'>
+                MyName
+              </Link>
+            </div>
+            <div className='header__user-img'>
+              <Link to='profile' className='header__user-gray'></Link>
+              <img src={defaulAvatar} alt='default user avatar' />
+            </div>
+            <Button
+              style={ButtonStyles.DELETE}
+              kind={ButtonKinds.INFO}
+              className='header__user-logout'
+            >
+              Logout
+            </Button>
+          </div>
         </div>
       </div>
     </header>
