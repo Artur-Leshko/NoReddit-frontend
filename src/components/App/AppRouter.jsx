79bcf4d0187a76d1e1ee39b61306b2282d31178e
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-17 00:08:07 +0300

    change routing with react-router-dom

--- src/components/App/AppRouter.jsx
+++ src/components/App/AppRouter.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate, } from 'react-router-dom';
 import { NotFound, ProtectedRoute, } from '../../common';
 import { Layout, } from '../Layout';
 import { Login, Register, } from '../User';
-import { Profile, } from '../Profile';
+import { ProfileLayout, } from '../Profile';
 
 const AppRouter = () => {
   return (
@@ -15,7 +15,7 @@ const AppRouter = () => {
         <Route element={<ProtectedRoute />}>
           <Route path='noreddit' element={<Layout />} >
             <Route index element={<div>Hi!</div>} />
-            <Route path='profile/:profileId' element={<Profile />} />
+            <Route path='profile/:profileId/*' element={<ProfileLayout />} />
           </Route>
         </Route>
         <Route path='*' element={<NotFound />} />
