4d59251dd503e0e06809247f11df9daf5ec64d0b
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-18 12:18:49 +0300

    create requests for (un-)subscription with axios lib

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,8 @@
 export { signIn, singUp, } from './auth';
-export { getSelfUserpofile, getAnyUserProfile, getFollowers, getFollowed, } from './userprofile';
+export {
+  getSelfUserpofile,
+  getAnyUserProfile,
+  getFollowers, getFollowed,
+  subscribeOnUser,
+  unsubscribeFromUser,
+} from './userprofile';
