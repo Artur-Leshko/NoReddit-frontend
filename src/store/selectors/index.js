f0796b9f30b6d44e2ccb68af4a081ee69c6d904e
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-18 22:04:59 +0300

    create selectors for new data from redux

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -2,3 +2,4 @@ export const userSelector = state => state.currentUser;
 export const anyUserSelector = state => state.user;
 export const followersSelector = state => state.followers;
 export const followedSelector = state => state.followed;
+export const userFollowedSelector = state => state.currentUserFollowed;
