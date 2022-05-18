00c39bf5b76cc77b43eb798690864e5c8b88b1bd
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-18 22:02:35 +0300

    add actions for followers reducer

--- src/store/actions/subscriptions.js
+++ src/store/actions/subscriptions.js
@@ -1,12 +1,28 @@
 import {
   UPDATE_FOLLOWERS,
   UPDATE_FOLLOWED,
+  ADD_FOLLOWER,
+  REMOVE_FOLLOWER,
 } from '../constants';
 
 export const updateFollowers = (dispatch, followers) => {
   dispatch({
     type: UPDATE_FOLLOWERS,
     payload: { followers, },
+  });
+};
+
+export const addFollower = (dispatch, follower) => {
+  dispatch({
+    type: ADD_FOLLOWER,
+    payload: { follower, },
+  });
+};
+
+export const removeFollower = (dispatch, follower) => {
+  dispatch({
+    type: REMOVE_FOLLOWER,
+    payload: { follower, },
   });
 };
 
