37b3b0d1e8049344eeb0b13d7be9402af64e7d3d
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-18 22:04:26 +0300

    update followersReducer

--- src/store/reducers/followersReducer.js
+++ src/store/reducers/followersReducer.js
@@ -1,5 +1,7 @@
 import {
   UPDATE_FOLLOWERS,
+  ADD_FOLLOWER,
+  REMOVE_FOLLOWER,
 } from '../constants';
 
 export default function followersReducer(state = [], data) {
@@ -8,6 +10,12 @@ export default function followersReducer(state = [], data) {
   switch (type) {
     case UPDATE_FOLLOWERS: {
       return [...payload.followers,];
+    }
+    case ADD_FOLLOWER: {
+      return [...state, { ...payload.follower, },];
+    }
+    case REMOVE_FOLLOWER: {
+      return [...state.filter(u => u.id !== payload.follower.id),];
     }
     default:
       return state;
