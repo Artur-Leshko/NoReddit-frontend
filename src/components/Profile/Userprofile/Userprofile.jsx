8fa686fc995c13fa9b5ea7b5cb6010e636d26828
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-18 22:06:30 +0300

    implement (un-)subscribe functionality

--- src/components/Profile/Userprofile/Userprofile.jsx
+++ src/components/Profile/Userprofile/Userprofile.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
-import { Input, InputKinds, Button, ButtonKinds, ButtonStyles, Loader, } from '../../../common';
+import React, { useState, } from 'react';
+import { useDispatch, } from 'react-redux';
+import { subscribeOnUser, unsubscribeFromUser, } from '../../../api';
+import { updateUser, addCurrentUserFollowed, removeCurrentUserFollowed, addFollower, removeFollower, } from '../../../store/actions';
+import { Input, InputKinds, Button, ButtonKinds, ButtonStyles, } from '../../../common';
 import defaulAvatar from '../../../images/default_avatar.png';
 import './userprofile.scss';
 
-export const Userprofile = ({ user, profileId, currentId, }) => {
+export const Userprofile = ({ currentUser, user, currentUserFollowed, profileId, currentId, }) => {
+  const [isFollowing, setIsFollowing,] = useState(Boolean(currentUserFollowed.find(user => user.id === profileId)));
+
+  const dispatch = useDispatch();
+
+  const onSubscribe = (e) => {
+    e.preventDefault();
+
+    if (isFollowing) {
+      unsubscribeFromUser(profileId)
+        .then(user => {
+          updateUser(dispatch, user);
+          removeCurrentUserFollowed(dispatch, user);
+          removeFollower(dispatch, currentUser);
+          setIsFollowing(false);
+        });
+    } else {
+      subscribeOnUser(profileId)
+        .then(user => {
+          updateUser(dispatch, user);
+          addCurrentUserFollowed(dispatch, user);
+          addFollower(dispatch, currentUser);
+          setIsFollowing(true);
+        });
+    }
+  };
+
   return (
     <div className='profile__user'>
       <div className='profile__user-avatar'>
@@ -30,6 +59,19 @@ export const Userprofile = ({ user, profileId, currentId, }) => {
           <div className='profile__user-title'>Surname:</div>
           <div className='profile__user-surname'>{user?.surname || 'Unknown'}</div>
         </div>
+        {currentId !== profileId ?
+          <div className='profile__user-item'>
+            <Button
+              kind={ButtonKinds.LOGIN}
+              style={isFollowing ? ButtonStyles.DELETE : ButtonStyles.SUCCESS}
+              className='profile__user-subscribe'
+              onClick={onSubscribe}
+            >
+              {isFollowing ? 'Unsubscribe' : 'Subscribe'}
+            </Button>
+          </div>
+          : null
+        }
       </div>
     </div>
   );
