e4530dd18632325a6f8f89f785bdd6a1e2ac6b03
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-20 14:26:37 +0300

    implement userprofile update of all the fields

--- src/components/Profile/Userprofile/Userprofile.jsx
+++ src/components/Profile/Userprofile/Userprofile.jsx
@@ -1,14 +1,23 @@
-import React, { useState, } from 'react';
+import React, { useState, useRef, } from 'react';
 import { useDispatch, } from 'react-redux';
-import { subscribeOnUser, unsubscribeFromUser, } from '../../../api';
-import { updateUser, addCurrentUserFollowed, removeCurrentUserFollowed, addFollower, removeFollower, } from '../../../store/actions';
-import { Input, InputKinds, Button, ButtonKinds, ButtonStyles, } from '../../../common';
+import { subscribeOnUser, unsubscribeFromUser, updateUserProfile, } from '../../../api';
+import {
+  updateCurrentUser,
+  updateUser,
+  addCurrentUserFollowed,
+  removeCurrentUserFollowed,
+  addFollower,
+  removeFollower,
+} from '../../../store/actions';
+import { Button, ButtonKinds, ButtonStyles, EditText, validateUsernameForm, } from '../../../common';
 import defaulAvatar from '../../../images/default_avatar.png';
 import './userprofile.scss';
 
 export const Userprofile = ({ currentUser, user, currentUserFollowed, profileId, currentId, }) => {
   const [isFollowing, setIsFollowing,] = useState(Boolean(currentUserFollowed.find(user => user.id === profileId)));
+  const [avatar, setAvatar,] = useState(user.avatar);
 
+  const inputRef = useRef();
   const dispatch = useDispatch();
 
   const onSubscribe = (e) => {
@@ -33,12 +42,37 @@ export const Userprofile = ({ currentUser, user, currentUserFollowed, profileId,
     }
   };
 
+  const onAvatartChange = (e) => {
+    const file = e.target.files[0];
+    onSave(e, 'avatar', file);
+  };
+
+  const onSave = (e, key, data) => {
+    e.preventDefault();
+
+    let bodyFormData = new FormData();
+    bodyFormData.append(key, data);
+
+    updateUserProfile(bodyFormData)
+      .then(currentUser => {
+        updateCurrentUser(dispatch, currentUser);
+        updateUser(dispatch, currentUser);
+      });
+  };
+
   return (
     <div className='profile__user'>
       <div className='profile__user-avatar'>
         <div className='profile__user-title'>Avatar</div>
         <div className='profile__user-img'>
-          <div className='profile__user-gray'></div>
+          <div className='profile__user-gray' onClick={e => currentId === profileId ? inputRef.current.click() : null}></div>
+          <input
+            type='file'
+            accept='.png,.jpg,.jpeg'
+            className='profile__avatar-input--hidden'
+            ref={inputRef}
+            onChange={e => onAvatartChange(e)}
+          />
           <img src={user.avatar ? 'http://127.0.0.1:8000' + user.avatar : defaulAvatar} alt='avatar' />
         </div>
         <div className='profile__user-subscriptions'>
@@ -48,16 +82,38 @@ export const Userprofile = ({ currentUser, user, currentUserFollowed, profileId,
       </div>
       <div className='profile__user-info'>
         <div className='profile__user-item'>
-          <div className='profile__user-title'>Username:</div>
-          <div className='profile__user-username'>{user.user.username}</div>
+          <EditText
+            defaultInfo={user.user.username}
+            infoType='username'
+            labelName='Username'
+            placeholder='Username'
+            classNamePrefix='profile__user'
+            isEditable={currentId === profileId}
+            validateFunc={validateUsernameForm}
+            onSave={onSave}
+          />
         </div>
         <div className='profile__user-item'>
-          <div className='profile__user-title'>Firstname:</div>
-          <div className='profile__user-firstname'>{user?.firstname || 'Unknown'}</div>
+          <EditText
+            defaultInfo={user?.firstname || 'Unknown'}
+            infoType='firstname'
+            labelName='Firstname'
+            placeholder='Firstname'
+            classNamePrefix='profile__user'
+            isEditable={currentId === profileId}
+            onSave={onSave}
+          />
         </div>
         <div className='profile__user-item'>
-          <div className='profile__user-title'>Surname:</div>
-          <div className='profile__user-surname'>{user?.surname || 'Unknown'}</div>
+          <EditText
+            defaultInfo={user?.surname || 'Unknown'}
+            infoType='surname'
+            labelName='Surname'
+            placeholder='Surname'
+            classNamePrefix='profile__user'
+            isEditable={currentId === profileId}
+            onSave={onSave}
+          />
         </div>
         {currentId !== profileId ?
           <div className='profile__user-item'>
@@ -73,6 +129,6 @@ export const Userprofile = ({ currentUser, user, currentUserFollowed, profileId,
           : null
         }
       </div>
-    </div>
+    </div >
   );
 };
