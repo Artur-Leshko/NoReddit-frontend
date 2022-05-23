d13f1b97067be2869d6bc8eeccf31a8914a10e41
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-23 16:55:58 +0300

    add selector for userPosts

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -4,3 +4,4 @@ export const followersSelector = state => state.followers;
 export const followedSelector = state => state.followed;
 export const userFollowedSelector = state => state.currentUserFollowed;
 export const popularPostsSelector = state => state.popularPosts;
+export const userPostsSelector = state => state.userPosts;
