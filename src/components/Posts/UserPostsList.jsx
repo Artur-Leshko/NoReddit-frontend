07b629ccb1a5e65e46cf96fdb0805d49f4dad1c7
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-23 23:28:08 +0300

    implement redux-store updating on voting posts

--- src/components/Posts/UserPostsList.jsx
+++ src/components/Posts/UserPostsList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { useDispatch, } from 'react-redux';
+import { useDispatch, useSelector, } from 'react-redux';
 import { useNavigate, } from 'react-router-dom';
 import { Button, ButtonKinds, ButtonStyles, } from '../../common';
 import { upvotePost, downvotePost, } from '../../api';
+import {
+  addUpvotedPost,
+  addDownvotedPost,
+  removeUpvotedPost,
+  removeDownvotedPost,
+} from '../../store/actions';
+import { upvotedPostsSelector, downvotedPostsSelector, } from '../../store/selectors';
 import defaultAvatar from '../../images/default_avatar.png';
 import './userpostslist.scss';
 
 export const UserPostsList = ({ title, posts, needBtn, fixedBtn, main, updatePost, }) => {
+  const upvotedPosts = useSelector(upvotedPostsSelector);
+  const downvotedPosts = useSelector(downvotedPostsSelector);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -15,6 +24,14 @@ export const UserPostsList = ({ title, posts, needBtn, fixedBtn, main, updatePos
     e.preventDefault();
 
     navigate(`/posts/${postId}`);
+  };
+
+  const isPostUpvoted = (post) => {
+    return upvotedPosts.find(p => p.id === post.id);
+  };
+
+  const isPostDownvoted = (post) => {
+    return downvotedPosts.find(p => p.id === post.id);
   };
 
   return (
@@ -26,20 +43,36 @@ export const UserPostsList = ({ title, posts, needBtn, fixedBtn, main, updatePos
             const avatartSrc = post.owner.avatar ?
               (post.owner.avatar.startsWith('http') ? post.owner.avatar : 'http://localhost:8000' + post.owner.avatar)
               : defaultAvatar;
+            const upvoted = isPostUpvoted(post);
+            const downvoted = upvoted ? false : isPostDownvoted(post);
+            const className = upvoted ? 'up' : downvoted ? 'down' : '';
+
             return (
-              <li key={post.id}>
-                <div className='posts__list-arrows'>
+              <li className={className} key={post.id}>
+                <div className={`posts__list-arrows ${className && 'posts__list-arrows--' + className}`}>
                   <div className='posts__arrows-group'>
                     <div className='posts__arrows-count posts__arrows-count--up'>{post.upvotes}</div>
                     <div className='posts__arrows-up' onClick={async (e) => {
                       const updatedPost = await upvotePost(post.id);
                       updatePost(dispatch, updatedPost);
+                      if (isPostDownvoted(post)) {
+                        removeDownvotedPost(dispatch, updatedPost);
+                        addUpvotedPost(dispatch, updatedPost);
+                      }
+                      else if (isPostUpvoted(post)) removeUpvotedPost(dispatch, updatedPost);
+                      else addUpvotedPost(dispatch, updatedPost);
                     }}></div>
                   </div>
                   <div className='posts__arrows-group'>
                     <div className='posts__arrows-down' onClick={async (e) => {
                       const updatedPost = await downvotePost(post.id);
                       updatePost(dispatch, updatedPost);
+                      if (isPostUpvoted(post)) {
+                        removeUpvotedPost(dispatch, updatedPost);
+                        addDownvotedPost(dispatch, updatedPost);
+                      }
+                      else if (isPostDownvoted(post)) removeDownvotedPost(dispatch, updatedPost);
+                      else addDownvotedPost(dispatch, updatedPost);
                     }}></div>
                     <div className='posts__arrows-count posts__arrows-count--down'>{post.downvotes}</div>
                   </div>
