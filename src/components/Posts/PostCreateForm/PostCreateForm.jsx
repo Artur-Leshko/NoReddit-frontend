fd0a877c911d4c94df0e3bf543229ec7afd43e37
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-24 22:38:12 +0300

    create PostCreateForm component

---
+++ src/components/Posts/PostCreateForm/PostCreateForm.jsx
@@ -0,0 +1,114 @@
+import React, { useState, } from 'react';
+import { useNavigate, } from 'react-router-dom';
+import { createPost, } from '../../../api';
+import {
+  Button,
+  ButtonKinds,
+  ButtonStyles,
+  Input,
+  InputKinds,
+  Textarea,
+  TextareaKinds,
+  ErrorBlock,
+  validatePostForm,
+  canBeSubmited,
+} from '../../../common';
+import './postcreate.scss';
+
+export const PostCreateForm = () => {
+  const [title, setTitle,] = useState('');
+  const [mainText, setMainText,] = useState('');
+  const [isPostBtnVisible, setIsPostBtnVisible,] = useState(false);
+  const [createdPost, setCreatedPost,] = useState(null);
+  const [errors, setErrors,] = useState({
+    title: [],
+    mainText: [],
+  });
+
+  const navigate = useNavigate();
+
+  const onSave = (e) => {
+    e.preventDefault();
+
+    const newErrors = validatePostForm({ title, mainText, });
+    setErrors(newErrors);
+
+    if (canBeSubmited(newErrors)) {
+      createPost({ title, main_text: mainText, categories: [], }).then(createdPost => {
+        setCreatedPost(createdPost);
+        setIsPostBtnVisible(true);
+      });
+    }
+  };
+
+  const onCancel = (e) => {
+    e.preventDefault();
+    navigate(-1);
+  };
+
+  const onPostBtnClick = (e) => {
+    e.preventDefault();
+    navigate(`/noreddit/posts/${createdPost.id}`);
+  };
+
+  return (
+    <div className='post__create'>
+      <div className='post__create-subtitle'>Create your new post!</div>
+      <form className='post__create-form'>
+        <label className='post__create-title'>Title:
+          <Input
+            disabled={isPostBtnVisible}
+            kind={InputKinds.INFO}
+            placeholder='Title'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </label>
+        <ErrorBlock errorArr={errors.title} id='title' />
+        <label className='post__create-text'>Text:
+          <Textarea
+            disabled={isPostBtnVisible}
+            kind={TextareaKinds.INFO}
+            placeholder='Text'
+            value={mainText}
+            onChange={e => setMainText(e.target.value)}
+          />
+        </label>
+        <ErrorBlock errorArr={errors.mainText} id='text' />
+        {isPostBtnVisible ? <div className='post__create-success'>Post created!</div> : null}
+        <div className='post__create-btns'>
+          {!isPostBtnVisible ?
+            <>
+              <Button
+                kind={ButtonKinds.INFO}
+                style={ButtonStyles.SUCCESS}
+                className='post__create-save'
+                onClick={onSave}
+              >
+                Save
+              </Button>
+              <Button
+                kind={ButtonKinds.INFO}
+                style={ButtonStyles.CANCEL}
+                className='post__create-cancel'
+                text='Back'
+                onClick={onCancel}
+              >
+                Back
+              </Button>
+            </>
+            : <Button
+              kind={ButtonKinds.INFO}
+              style={ButtonStyles.SUCCESS}
+              className='post__create-continue'
+              text='Go to new post!'
+              onClick={onPostBtnClick}
+            >
+              Go to new post!
+            </Button>
+          }
+        </div>
+      </form>
+    </div>
+  );
+};
