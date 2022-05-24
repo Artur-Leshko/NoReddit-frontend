9e4cbcda04458b55b3e416034a8684e5aa7f0ffa
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-24 11:32:56 +0300

    update exports

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,1 +1,3 @@
-export { UserPostsList, } from './UserPostsList';
+export { PostLayout, } from './PostLayout';
+export { UserPostsList, } from './UserPostsList/UserPostsList';
+export { SeparatePost, } from './SeparatePost/SeparatePost';
