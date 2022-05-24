935ab70d1995d4e41e0703e96b82b39cf1e993b3
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-24 12:37:15 +0300

    create EditTextarea common component

---
+++ src/common/components/EditComponents/EditTextarea.jsx
@@ -0,0 +1,72 @@
+import React, { useState, } from 'react';
+import { Textarea, TextareaKinds, Button, ButtonKinds, ButtonStyles, ErrorBlock, } from '../../index';
+
+export const EditTextarea = ({ defaultInfo = '', infoType, labelName, placeholder,
+  classNamePrefix, isEditable, validateFunc = undefined, onSave, }) => {
+
+  const [editMode, setEditMode,] = useState(false);
+  const [error, setError,] = useState({ [infoType]: [], });
+  const [info, setInfo,] = useState(defaultInfo);
+
+  return (
+    <>
+      {labelName ? <div className={`${classNamePrefix}-title`}>{labelName}:</div> : null}
+      {!editMode ?
+        <div className={classNamePrefix + '-' + infoType}>{defaultInfo}</div>
+        : <Textarea
+          kind={TextareaKinds.INFO}
+          type='text'
+          value={info}
+          placeholder={placeholder}
+          onChange={(e) => setInfo(e.target.value)}
+        />
+      }
+      <div className={`${classNamePrefix}-errors`}>
+        <ErrorBlock errorArr={error[infoType]} id={infoType} />
+      </div>
+      {isEditable ?
+        <div className={`${classNamePrefix}-btns`}>
+          {!editMode ?
+            <Button
+              kind={ButtonKinds.INFO}
+              style={ButtonStyles.CANCEL}
+              className={`${classNamePrefix}-edit`}
+              onClick={(e) => setEditMode(true)}
+            >
+              Edit
+            </Button>
+            : <>
+              <Button
+                className={`${classNamePrefix}-save`}
+                kind={ButtonKinds.INFO}
+                style={ButtonStyles.SUCCESS}
+                onClick={async (e) => {
+                  let newErrors = validateFunc ? validateFunc(info) : { [infoType]: [], };
+                  setError(newErrors);
+
+                  if (newErrors[infoType].length === 0) {
+                    await onSave(e, infoType, info);
+                    setEditMode(false);
+                  }
+                }}
+              >
+                Save
+              </Button>
+              <Button
+                className={`${classNamePrefix}-cancel`}
+                kind={ButtonKinds.INFO}
+                style={ButtonStyles.CANCEL}
+                onClick={(e) => {
+                  setError({ [infoType]: [], });
+                  setEditMode(false);
+                }}
+              >
+                Cancel
+              </Button>
+            </>}
+        </div>
+        : null
+      }
+    </>
+  );
+};
