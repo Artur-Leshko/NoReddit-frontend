72e32712e1f367c710b906ad7ffa908c0acf9e09
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-24 11:31:51 +0300

    update routing with react-dom-routee lib

--- src/components/App/AppRouter.jsx
+++ src/components/App/AppRouter.jsx
@@ -5,6 +5,7 @@ import { Layout, } from '../Layout';
 import { Login, Register, } from '../User';
 import { MainPage, } from '../MainPage';
 import { ProfileLayout, } from '../Profile/ProfileLayout';
+import { PostLayout, } from '../Posts';
 
 const AppRouter = () => {
   return (
@@ -17,6 +18,7 @@ const AppRouter = () => {
           <Route path='noreddit' element={<Layout />} >
             <Route index element={<MainPage />} />
             <Route path='profile/:profileId/*' element={<ProfileLayout />} />
+            <Route path='posts/*' element={<PostLayout />} />
           </Route>
         </Route>
         <Route path='*' element={<NotFound />} />
