674da77055850af38eddcab2203221f408d05151
Artur Leshko <fire.ghost.01@mail.ru>
2022-05-24 22:39:16 +0300

    update routing in post layout

--- src/components/Posts/PostLayout.jsx
+++ src/components/Posts/PostLayout.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate, } from 'react-router-dom';
 import { useSelector, } from 'react-redux';
 import { userSelector, } from '../../store/selectors';
 import { SeparatePost, } from './SeparatePost/SeparatePost';
+import { PostCreateForm, } from './PostCreateForm/PostCreateForm';
 import './postlayout.scss';
 
 export const PostLayout = () => {
@@ -14,7 +15,7 @@ export const PostLayout = () => {
         <div className='post__inner'>
           <Routes>
             <Route path=':postId' element={<SeparatePost currentUser={currentUser} />} />
-            <Route path='create' element={<div>Kuku</div>} />
+            <Route path='create' element={<PostCreateForm />} />
             <Route path='*' element={<Navigate to='/not-found' replace />} />
           </Routes>
         </div>
